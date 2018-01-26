c411d231798890021689a0f2e40df91e19fd825f
Ajwinder Singh <asinghgr@srv601.webhostingforstudents.com>
2018-01-26 10:29:06 -0500

    Basic setup done

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+
 //Require the autoload file
 require_once('vendor/autoload.php');
 
 //Create an instance of the Base Class
 $f3 = Base :: instance();
+//$f3 = set('colors', array('pink', 'green', 'blue'));
 
-//Define a default rout
-$f3->route('GET /', function ()
+//Define a default route
+$f3->route('GET|POST /new-pet', function ($f3)
 {
-    $view = new View;
-    echo $view->render
-    ('views/home.html');
-    //echo '<h1>Hello, world!</h1>';
+    $f3->set('username','aj');
+
+    $f3->set('colors', array('pink','green', 'blue'));
+    $template = new Template();
+    echo $template->render('views/home.html');
 }
 );
 
 //Run fat free
 $f3->run();
-?>
---
+++ model/validate.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ajwinder
+ * Date: 1/26/18
+ * Time: 7:19 AM
+ */
+
+function validColor($color)
+{
+    global $f3;
+    return in_array($color, $f3->get('colors'));
+}
+
+function validString($str)
+{
+    if (!empty($str))
+    {
+        return true;
+    }
+}
+
+
+$errors = array();
+
+if (!validColor($color))
+{
+    $errors['color'] = "Please enter a valid color";
+
+}
