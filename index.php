de3a80ffead0ec9c6f3205790db362ccaa4a3d65
Ajwinder Singh <asinghgr@srv601.webhostingforstudents.com>
2018-01-30 20:19:47 -0500

    Final Changes

--- index.php
+++ index.php
@@ -8,36 +8,34 @@ require_once('vendor/autoload.php');
 
 //Create an instance of the Base Class
 $f3 = Base :: instance();
-//$f3 = set('colors', array('pink', 'green', 'blue'));
 
-//Define a default route
-$f3->route('GET|POST /', function ($f3)
+$f3-> set('colors', array('pink', 'green', 'blue'));
+
+$f3-> set('DEBUG', 3);
+
+$f3->route('GET|POST /new-pet', function($f3)
 {
-    //$f3->set('username','aj');
-    $f3->set('colors', array('pink','green', 'blue'));
-
 
     if (isset($_POST['submit']))
     {
-        $color = $_POST['pet-color'];
-        $petname = $_POST['pet-name'];
-        $pettype = $_POST['pet-type'];
+        //print_r($_POST);
+        $color = $_POST['pcolor'];
+        $petname = $_POST['pname'];
+        $pettype = $_POST['ptype'];
 
-        include ('model/validate.php');
+        require ('model/validate.php');
 
-        $f3->set('pet-color',$color);
-        $f3->set('pet-name',$petname);
-        $f3->set('pet-type',$pettype);
-        $f3->set('success',success);
-        $success = get('success');
-
-
+        $f3->set('color',$color);
+        $f3->set('pname',$petname);
+        $f3->set('ptype',$pettype);
+        $f3->set('success',$success);
+        $f3->set('errors', $errors);
     }
 
     $template = new Template();
     echo $template->render('views/home.html');
-}
-);
+
+});
 
 //Run fat free
 $f3->run();
--- model/validate.php
+++ model/validate.php
@@ -12,21 +12,29 @@ function validColor($color)
     return in_array($color, $f3->get('colors'));
 }
 
-function validString($str)
+function validString($pname)
 {
-    if (!empty($str) && ctype_alpha($str))
+    if (!empty($pname) && ctype_alpha($pname))
     {
         return true;
     }
 }
 
-
 $errors = array();
 
-if (!validColor($color))
+if(!validString($petname))
+{
+    $errors['pname'] = "Please enter a valid name";
+}
+
+if(!validColor($color))
 {
     $errors['color'] = "Please enter a valid color";
+}
 
+if(!validString($pettype))
+{
+    $errors['ptype'] = "Please enter a valid type";
 }
 
 $success = sizeof($errors) == 0;
